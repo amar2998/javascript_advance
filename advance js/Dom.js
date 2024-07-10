eccf763a440b51fd843e4f1add0455b8b73dfe06
amar2998 <160626597+amar2998@users.noreply.github.com>
2024-07-10 13:27:50 +0000

    commit

--- advance js/Dom.js
+++ advance js/Dom.js
@@ -1,1 +1,22 @@
-console.log("amar sonu nath")
+//reduce
+let arr=[1,2,3,4,5,6,7,8,9,10]
+arr.map((value,index)=>{
+    // console.log(value)//we can print the value 
+    // console.log(index)//we can print the index
+
+})
+let num2=arr.filter((i)=>{
+    if(i<4){
+        return i
+    }
+
+})
+// console.log(num2)
+
+
+//rduce
+let num4=arr.reduce((h1,h2)=>{
+    return h1+h2
+
+})
+console.log(num4)
