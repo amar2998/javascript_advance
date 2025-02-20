524b01ddbf98db90e831580d88ae046c3c0c288e
amar2998 <160626597+amar2998@users.noreply.github.com>
2025-02-20 16:10:11 +0000

    first

--- array.js
+++ array.js
@@ -1,26 +1,51 @@
-const arr=[9,8,7,6,5,4,3,2,1]
-let arr2=arr.sort()
-console.log(typeof arr)
-//array is an object
-const str="i ma amar"
-let str2=str.split('');
-console.log(str2)
-let arraystring=["hi ","my","name","is","amar"]
-console.log(arraystring[3][0])
+// const arr=[9,8,7,6,5,4,3,2,1]
+// let arr2=arr.sort()
+// console.log(typeof arr)
+// //array is an object
+// const str="i ma amar"
+// let str2=str.split('');
+// console.log(str2)
+// let arraystring=["hi ","my","name","is","amar"]
+// console.log(arraystring[3][0])
 
-const arr3= new Array(1,2,3,4,5)
-arr3.push(45)
-arr3.pop()
-const ans=arr3.shift();
-const ans2=arr3.unshift("shaktiman");
+// const arr3= new Array(1,2,3,4,5)
+// arr3.push(45)
+// arr3.pop()
+// const ans=arr3.shift();
+// const ans2=arr3.unshift("shaktiman");
 
-// console.log(arr3,ans)
-console.log(arr3.includes("shaktiman"))
-console.log(arr3.indexOf("shaktiman"))
+// // console.log(arr3,ans)
+// console.log(arr3.includes("shaktiman"))
+// console.log(arr3.indexOf("shaktiman"))
 
-console.log(arr3.join())//arr to string
+// console.log(arr3.join())//arr to string
 
-console.log(arr3.slice(0,3))
-console.log(arr3)
-console.log(arr3.splice(0,3))
-console.log(arr3)
+// console.log(arr3.slice(0,3))
+// console.log(arr3)
+// console.log(arr3.splice(0,3))
+// console.log(arr3)
+
+const strarr=[1,2,3,4,5,6,7,8]
+console.log(strarr.join(" "))
+const arr1=[8,7,87,5,65,46]
+arr1.push(23)
+console.log(arr1)
+arr1.pop();
+console.log(arr1)
+console.log(arr1.concat(strarr));
+console.log([...arr1,...strarr])
+const string="amarsonunath"
+console.log(Array.from(string))
+let a=23
+let b=23
+let c=23
+let d=23
+let e=23
+console.log(Array.of(a,b,c,d,e))
+
+
+
+
+
+
+
