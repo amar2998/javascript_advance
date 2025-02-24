a62c55f4feb0004f65ef2f9ae6330c726be1ee49
amar2998 <160626597+amar2998@users.noreply.github.com>
2025-02-24 15:38:04 +0000

    "second"

---
+++ objects.js
@@ -0,0 +1,24 @@
+const sym=Symbol("key1")
+const app="name";
+const obj={
+    [sym]:"key no one",
+    [app]:"amar sonu"
+
+
+}
+obj.func=function(){
+    return (`hi this is ${this[app]}`); // if we do not return anythig the js will give undefine
+    
+}
+console.log(obj.func())
+const ob1={
+    1:"a",
+    2:"b"
+}
+const ob2={
+    3:"c",
+    4:"d"
+}
+const ob=Object.assign({},ob1,ob2);
+const on4={...ob1,...ob2}
+console.log(on4);
